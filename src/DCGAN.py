15481eca3bae45b9f1bd225941a0733acafad9d5
Junhong Xu <jxu7@bsu.edu>
2017-01-29 11:40:35 -0500

    finished building the discriminator and generator

--- src/DCGAN.py
+++ src/DCGAN.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
-
+from src.ops import *
 
 class DCGAN(object):
     """
     An implementation of DCGAN
     """
-    def __init__(self, input_dim=(64, 64, 3), z_dim=100, batch_size=64, lr=1e-4):
+    def __init__(self, input_dim=(64, 64, 3), z_dim=100, batch_size=64, lr=1e-4, init_num_kernels=64):
         """
         Parameters:
             input_dim: a tuple of (H, W, C) of input image shape to the discriminator
@@ -20,6 +20,7 @@ class DCGAN(object):
         self.z_dim = z_dim
         self.batch_size = batch_size
         self.lr = lr
+        self.init_num_kernels = init_num_kernels
 
         # input to D
         self.x = tf.placeholder(dtype=tf.float32, shape=(None, ) + input_dim, name="x")
@@ -28,20 +29,71 @@ class DCGAN(object):
         self.z = tf.placeholder(dtype=tf.float32, shape=(None, z_dim), name="z")
 
         # the output of the generator
-        self.G = self.generator()
+        self.G = self.generator(self.z, reuse=None, is_train=True)
 
         # the probability of input is from real data
-        self.D1 = self.discriminator(self.x, reuse=None)
+        self.D1 = self.discriminator(self.x, reuse=None, is_train=True)
         # the probability of input is from fake data
-        self.D2 = self.discriminator(self.z, reuse=True)
+        self.D2 = self.discriminator(self.G, reuse=True, is_train=True)
+
+        self.g_params = [param for param in tf.trainable_variables() if 'generator' in param]
+        self.d_params = [param for param in tf.trainable_variables() if 'discriminator' in param]
 
         # build the whole model and loss
         self.fake_data_loss, self.real_data_loss, self.generator_loss, self.d_optimizer, self.g_optimizer = self.build()
 
-    def discriminator(self, inpt, reuse):
+    def discriminator(self, inpt, reuse, is_train):
         """
-        Build D. If reuse if True, the input should be the output of generator
+        Build D for training or testing. If reuse if True, the input should be the output of generator
         """
         with tf.variable_scope("discriminator"):
-            net = tf.nn.conv2d(input=inpt,)
-            pass
+            if reuse:
+                tf.get_variable_scope().reuse_variables()
+            net = conv2d(x=inpt, num_kernels=self.init_num_kernels, name="conv1", activation=lkrelu, padding="SAME",
+                         alpha=0.02, is_train=is_train)
+            net = conv2d(x=net, num_kernels=self.init_num_kernels*2, name="conv2", activation=lkrelu, padding="SAME",
+                         alpha=0.02, is_train=is_train)
+            net = conv2d(x=net, num_kernels=self.init_num_kernels*4, name="conv3", activation=lkrelu, padding="SAME",
+                         alpha=0.02, is_train=is_train)
+            net = conv2d(x=net, num_kernels=self.init_num_kernels*8, name="conv4", activation=lkrelu, padding="SAME",
+                         alpha=0.02, is_train=is_train)
+            net = dense_layer(x=net, num_neurons=1, name="output", activation=tf.identity, is_train=is_train)
+        return net
+
+    def generator(self, inpt, reuse, is_train):
+        """
+        Build generator G for training or testing
+        """
+        h, w, c = self.input_dim
+        with tf.variable_scope("generator"):
+            if reuse:
+                tf.get_variable_scope().reuse_variables()
+            net = dense_layer(inpt, self.init_num_kernels*h**2/32, activation=tf.nn.relu,
+                              name="dense_input", use_bn=True, is_train=is_train)
+            # reshape the output of dense layer to be H/16, W/16, K*8
+            net = tf.reshape(net, (-1, h//16, w//16, self.init_num_kernels*8))
+            net = transpose_conv2d(net, self.init_num_kernels*4, h//8, w//8, name="trans_conv1", is_train=is_train)
+            net = transpose_conv2d(net, self.init_num_kernels*2, h//4, w//4, name="trans_conv2", is_train=is_train)
+            net = transpose_conv2d(net, self.init_num_kernels, h//2, w//2, name="trans_conv3", is_train=is_train)
+            net = transpose_conv2d(net, c, h, w, name="trans_conv4", is_train=is_train, activation=tf.nn.tanh)
+        return net
+
+    def build(self):
+        g_optimizer = tf.train.AdamOptimizer(learning_rate=self.lr)
+        with tf.variable_scope("discriminator_optimizer"):
+            d_optimizer = tf.train.AdamOptimizer(learning_rate=self.lr)
+            # loss for real data
+            real_data_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.D1, tf.ones_like(self.D1)))
+            # loss for fake data
+            fake_data_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.D2, tf.zeros_like(self.D2)))
+            d_loss = fake_data_loss + real_data_loss
+            d_optimize = d_optimizer.minimize(d_loss, var_list=self.d_params)
+
+        with tf.variable_scope("generator_optimizer"):
+            g_loss = tf.nn.sigmoid_cross_entropy_with_logits(self.D2, tf.ones_like(self.D2))
+            g_optimize = g_optimizer.minimize(g_loss, var_list=self.g_params)
+        return fake_data_loss, real_data_loss, g_loss, d_optimize, g_optimize
+
+
+if __name__ == '__main__':
+    DCGAN()
--- src/ops.py
+++ src/ops.py
@@ -2,21 +2,67 @@ import tensorflow as tf
 
 
 def conv2d(x, num_kernels, kernel_h=5, kernel_w=5, strides=2, padding="VALID", name="conv2d",
-           use_bn=True, activation=tf.nn.relu, is_train=True, stddv=0.02):
+           use_bn=True, activation=tf.nn.relu, alpha=None, is_train=True, stddv=0.02):
     """
     Wrapper function for convolutional layer
     """
-    reuse = True if not is_train else None
     n, h, w, c = x.get_shape().as_list()
-    with tf.variable_scope(name, reuse=reuse):
+    with tf.variable_scope(name):
         w = tf.get_variable(name="weight", initializer=tf.truncated_normal_initializer(stddev=stddv),
                             shape=(kernel_h, kernel_w, c, num_kernels))
         bias = tf.get_variable(name="bias", initializer=tf.constant_initializer(0.01), shape=num_kernels)
-        y = tf.nn.conv2d(x, w, (1, strides, strides, 1), padding, name=name)
+        y = tf.nn.conv2d(x, w, (1, strides, strides, 1), padding)
         y = tf.nn.bias_add(y, bias)
-        y = batch_norm(y, activation, tf.get_variable_scope().name, reuse, is_train)
-        return y
-    
+
+        if use_bn:
+            y = batch_norm(y, tf.get_variable_scope().name, is_train)
+
+        print("Convolutional 2D Layer %s, kernel size %s, output size %s Reuse:%s"
+              % (tf.get_variable_scope().name, (kernel_h, kernel_w, c, num_kernels), y.get_shape().as_list(),
+                 tf.get_variable_scope().reuse))
+        if alpha is None:
+            y = activation(y)
+        else:
+            y = activation(y, alpha)
+    return y
+
+
+def transpose_conv2d(x, num_kernels, new_h, new_w, kernel_h=5, activation=tf.nn.relu, kernel_w=5, stride=2,
+                     padding="VALID", use_bn=True, is_train=True, stddv=0.02, name="transpose_conv2d"):
+    n, h, w, c = x.get_shape().as_list()
+    with tf.variable_scope(name):
+        w = tf.get_variable(name="weight", initializer=tf.truncated_normal_initializer(stddev=stddv),
+                            shape=(kernel_h, kernel_w, num_kernels, c))
+        bias = tf.get_variable(name="bias", initializer=tf.constant_initializer(0.01), shape=num_kernels)
+        y = tf.nn.conv2d_transpose(x, w, output_shape=(-1, new_h, new_w, num_kernels), padding=padding,
+                                   strides=(1, stride, stride, 1))
+        y = tf.nn.bias_add(y, bias)
+        if use_bn:
+            y = batch_norm(y, tf.get_variable_scope().name, is_train)
+
+        print("Transposed Convolutional 2D Layer %s, kernel size %s, output size %s Reuse:%s"
+              % (tf.get_variable_scope().name, (kernel_h, kernel_w, c, num_kernels), y.get_shape().as_list(),
+                 tf.get_variable_scope().reuse))
+    return activation(y)
+
+
+def dense_layer(x, num_neurons, name, activation, use_bn=False, is_train=True):
+    if len(x.get_shape().as_list()) > 2:
+        n, h, w, c = x.get_shape().as_list()
+        d = h * w * c
+    else:
+        n, d = x.get_shape().as_list()
+    with tf.variable_scope(name):
+        # flatten x
+        x = tf.reshape(x, (-1, d))
+        w = tf.get_variable("weight", shape=(d, num_neurons), initializer=tf.random_normal_initializer(stddev=0.02))
+        b = tf.get_variable("bias", shape=num_neurons, initializer=tf.constant_initializer(0.01))
+        y = tf.matmul(x, w) + b
+        if use_bn:
+            y = batch_norm(y, name=tf.get_variable_scope().name, is_train=is_train)
+        print("Dense Layer %s, output size %s" % (tf.get_variable_scope().name, y.get_shape().as_list()))
+    return activation(y)
+
 
 def lkrelu(x, alpha, name="leaky_relu"):
     """
@@ -34,8 +80,12 @@ def lkrelu(x, alpha, name="leaky_relu"):
     return y
 
 
-def batch_norm(x, activation, name, reuse, is_train=True):
-    name = '%s/%s' % name, 'bn'
-    y = tf.contrib.layers.batch_norm(x, decay=0.9, center=True, scale=True, updates_collections=None, reuse=reuse,
-                                     is_training=is_train, scope=name, activation_fn=activation)
+def batch_norm(x, name, is_train=True):
+    name = '%s/%s' % (name, 'bn')
+    y = tf.contrib.layers.batch_norm(x, decay=0.9, center=True, scale=True, updates_collections=None,
+                                     is_training=is_train, scope=name)
+    print("Batch Normalization Layer %s. Reuse: %s. Is training %s" % (name, tf.get_variable_scope().reuse, is_train))
     return y
+
+
+
