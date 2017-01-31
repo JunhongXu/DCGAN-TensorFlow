487eeaac5078c02885c0c2c6376c784a0087d898
jxu7 <jxu7@bsu.edu>
2017-01-31 15:47:19 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# DCGNA Reimplementation in TensorFLow
+
+Link of the original paper: https://arxiv.org/abs/1511.06434
+
+![gan](misc/gen-architecture.png)
+
+
--- main.py
+++ main.py
@@ -11,5 +11,5 @@ data = np.lib.pad(data, ((0, 0), (2, 2), (2, 2), (0, 0)), "constant", constant_v
 plt.imshow(data[0].reshape(32, 32), cmap="gray")
 plt.show()
 with tf.Session() as sess:
-    dcgan = DCGAN(sess, input_dim=(32, 32, 1), g_init=32, d_init=8, name="mnist", lr=0.0001, stddv=0.01)
-    dcgan.train(data, 50, test_every=800)
+    dcgan = DCGAN(sess, input_dim=(32, 32, 1), g_init=64, d_init=32, name="mnist", lr=0.0001, stddv=0.02)
+    dcgan.train(data, 100, test_every=800)
